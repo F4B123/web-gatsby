3a62015ec615dcd1003b40f9a92ea929c8b5561c
fabianruizt4@outlook.com <fabianruizt4@outlook.com>
2022-02-14 13:32:05 -0500

    Gatsby project

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout2 from '../components/layout2'
+
+
+const AboutPage = () => {
+  return (
+    <Layout2 pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout2>
+  )
+}
+export default AboutPage
